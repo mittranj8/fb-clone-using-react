6baa5b864eca8a76d7a40c7a6ba5626a7d2d3935
Mittranj <mpansuriya@unomaha.edu>
2020-09-21 03:18:46 -0500

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVDsR9u45UPwlYtgegsCCYt0ktOxmQSUo",
+  authDomain: "fb-clone-react-5beba.firebaseapp.com",
+  databaseURL: "https://fb-clone-react-5beba.firebaseio.com",
+  projectId: "fb-clone-react-5beba",
+  storageBucket: "fb-clone-react-5beba.appspot.com",
+  messagingSenderId: "1069395834220",
+  appId: "1:1069395834220:web:47993c7feb8d7b70702f49",
+  measurementId: "G-V4NTL9ZBK0",
+};
