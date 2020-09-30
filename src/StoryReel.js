7d6e95d3c9850289eec4a66d72b87a7e68c1bd42
Mittranj <mpansuriya@unomaha.edu>
2020-09-29 22:43:25 -0500

    fixed story css

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -11,19 +11,19 @@ function StoryReel() {
         title="Dytto"
       />
       <Story
-        image="https://scontent-dfw5-2.xx.fbcdn.net/v/t15.5256-10/p480x480/120515708_253272012685738_403457168824738317_n.jpg?_nc_cat=100&_nc_sid=ad6a45&_nc_ohc=hBGvkLzJYPEAX9GzRz-&_nc_ht=scontent-dfw5-2.xx&oh=4355d7f78166fe14a8d4eba0cab591fd&oe=5F996823"
-        profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p200x200/49608154_1526306797514416_3574066312495759360_o.jpg?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=nIPuPWJ0o6UAX_CVWXg&_nc_ht=scontent-dfw5-2.xx&tp=6&oh=89e2d5723ab3a826e8de7418bf58e828&oe=5F9AF3BF"
-        title="Dytto"
+        image="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120292449_10151562699474987_3858630624721098390_o.jpg?_nc_cat=104&_nc_sid=5b7eaf&_nc_ohc=IUbNdXMuoIUAX8cQHo1&_nc_ht=scontent-dfw5-2.xx&oh=46d5201712781009cbda8538a143d226&oe=5F9899BD"
+        profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p200x200/59911883_10151265684944987_4991750779711782912_n.png?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=TG0Rt55S88YAX-lLkp1&_nc_ht=scontent-dfw5-2.xx&oh=70c8f758ddb5238fdf9234dd8e62c3cd&oe=5F98925C"
+        title="Ciara"
       />
       <Story
-        image="https://scontent-dfw5-2.xx.fbcdn.net/v/t15.5256-10/p480x480/120515708_253272012685738_403457168824738317_n.jpg?_nc_cat=100&_nc_sid=ad6a45&_nc_ohc=hBGvkLzJYPEAX9GzRz-&_nc_ht=scontent-dfw5-2.xx&oh=4355d7f78166fe14a8d4eba0cab591fd&oe=5F996823"
-        profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p200x200/49608154_1526306797514416_3574066312495759360_o.jpg?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=nIPuPWJ0o6UAX_CVWXg&_nc_ht=scontent-dfw5-2.xx&tp=6&oh=89e2d5723ab3a826e8de7418bf58e828&oe=5F9AF3BF"
-        title="Dytto"
+        image="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120200269_10158884698569581_3830128101482672225_o.jpg?_nc_cat=110&_nc_sid=5b7eaf&_nc_ohc=IPELbdS0_2wAX90kWIz&_nc_ht=scontent-dfw5-1.xx&oh=65d76a1700df66b7b32242658ede0fe2&oe=5F97C220"
+        profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p200x200/66176598_10157539302294581_3859427053493288960_n.png?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=SGxgjgVQDFkAX9m6RMN&_nc_ht=scontent-dfw5-2.xx&oh=53f9c0db58df1e619fa11993effdcbf4&oe=5F9B33F1"
+        title="Billboard"
       />
       <Story
-        image="https://scontent-dfw5-2.xx.fbcdn.net/v/t15.5256-10/p480x480/120515708_253272012685738_403457168824738317_n.jpg?_nc_cat=100&_nc_sid=ad6a45&_nc_ohc=hBGvkLzJYPEAX9GzRz-&_nc_ht=scontent-dfw5-2.xx&oh=4355d7f78166fe14a8d4eba0cab591fd&oe=5F996823"
-        profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p200x200/49608154_1526306797514416_3574066312495759360_o.jpg?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=nIPuPWJ0o6UAX_CVWXg&_nc_ht=scontent-dfw5-2.xx&tp=6&oh=89e2d5723ab3a826e8de7418bf58e828&oe=5F9AF3BF"
-        title="Dytto"
+        image="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120492812_10158053299808439_4674819332668076817_o.jpg?_nc_cat=101&_nc_sid=5b7eaf&_nc_ohc=1K7MZ5G8gusAX8K5Jmk&_nc_ht=scontent-dfw5-1.xx&oh=181225ac32b30498ab5189fe1ca5f795&oe=5F9B0EED"
+        profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/p200x200/63929249_10156698772358439_8635913797451644928_n.jpg?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=9_ldkgDiUEIAX_Hcssk&_nc_ht=scontent-dfw5-2.xx&tp=6&oh=28f99c911ae0e14395d78567566923a6&oe=5F9B4D6A"
+        title="Aditi Raval"
       />
     </div>
   );
